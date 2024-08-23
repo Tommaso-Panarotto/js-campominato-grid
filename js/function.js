b2120402e1be59e4c1744e9d84e0b9eaea0f7712
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-08-23 16:21:06 +0200

    create cell js

---
+++ js/function.js
@@ -0,0 +1,9 @@
+//funzione generazione celle
+function createCell() {
+    //creo la cella
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+
+    //restituisco la cella
+    return cell;
+}
---
+++ js/script.js
@@ -0,0 +1,29 @@
+/*
+0.recupero gli elementi dal DOM
+1. genero le celle al click del bottone
+2. inserisco le celle nel DOM
+3. riempo le celle con il contenuto
+4. reagisco al click sulla cella
+*/
+
+//FASE PREPARATORIA
+//recupero gli elementi dal DOM
+const grid = document.getElementById('grid');
+const button = document.querySelector('button');
+
+//numero celle
+const rows = 10;
+const cols = 10;
+const totalCells = rows * cols;
+
+//reagisco al click
+button.addEventListener('click', function () {
+    //genero il numero corretto di celle
+    for (let i = 0; i < totalCells; i++) {
+        const cell = createCell();
+
+        //la aggiungo in pagina
+        grid.appendChild(cell);
+    }
+
+})
